225ad6e9292ed1bc7ce03636fd28da1e627e2a00
qkrwldnsdlek-rgb <qkrwldnsdlek@gmail.com>
2026-04-23 07:10:47 +0900

    add supabase client

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://orytchawzooiabfwwqek.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yeXRjaGF3em9vaWFiZnd3cWVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NDc4NzMsImV4cCI6MjA5MjMyMzg3M30.JjdgQtIBmzD8qjh0TE2pYtL4DUbjEKORLnmFwgz8HVw';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
