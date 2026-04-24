597cc82c289d9e2d2964129664a9d6679e083ee4
qkrwldnsdlek-rgb <qkrwldnsdlek@gmail.com>
2026-04-24 21:08:02 +0900

    fix admin table td

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -109,8 +109,8 @@ function AdminPage() {
                   <th style={{padding: '14px', textAlign: 'left'}}>제목</th>
                   <th style={{padding: '14px', textAlign: 'center', width: '100px'}}>작성자</th>
                   <th style={{padding: '14px', textAlign: 'center', width: '100px'}}>카테고리</th>
-                  <th style={{padding: '14px', textAlign: 'center', width: '120px'}}>작성일</th>
-                  <th style={{padding: '14px', textAlign: 'center', width: '120px'}}>관리</th>
+                  <th style={{padding: '14px', textAlign: 'center', width: '100px'}}>작성일</th>
+                  <th style={{padding: '14px', textAlign: 'center', width: '100px'}}>관리</th>
                 </tr>
               </thead>
               <tbody>
