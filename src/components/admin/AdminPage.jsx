652afb55d624e5b8af5029555b58d85b7209bbd7
qkrwldnsdlek-rgb <qkrwldnsdlek@gmail.com>
2026-04-25 22:13:25 +0900

    r

--- src/components/admin/AdminPage.jsx
+++ src/components/admin/AdminPage.jsx
@@ -131,7 +131,6 @@ function AdminPage() {
   >
     <option value="전체">전체</option>
     <option value="제목">제목</option>
-    <option value="내용">내용</option>
     <option value="제목+내용">제목+내용</option>
   </select>
   <input
